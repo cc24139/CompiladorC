8990ffb742e81c648bd669323a8f84a3efb7760c
LFilipeLima <lf44654@gmail.com>
2026-05-16 20:54:14 -0300

    Refactor parser: global token & new parsing rules
    Bloco feito com suas funçoes complementares, expressao feito

--- AnalisadorSintatico.c
+++ AnalisadorSintatico.c
@@ -1,8 +1,10 @@
 #include "basics.h"
 #include "AnalisadorLexico.h"
 
+static Token token;
+
 void verificaProgam() {
-	Token token = anaLex();
+	 token = anaLex();
 	if (token != Programa) {
 		printf("Erro: esperava a palavra program!");
 		exit(1);
@@ -42,40 +44,41 @@ void verificaProgam() {
 	}
 }
 void verificaBloco() {
-	Token token = anaLex();
-	if(token == label){
-		AtribuirLabel();
+	token = anaLex();
+	while(token == rotulo){
+		Atribuirrotulo();
+		token = analex();
 	}
 
-	token = anaLex();
-    if (verificaType(Token) == false){
-        printf("Erro: Esperava um tipo");
-        exit(1);
-    }
     while(verificaType(token) == true){
         AtribuirVariavel();
         token = anaLex();
     }
-    if (token != implicito){
-        printf("Erro: Esperava implicito");
-		exit(1);
-    }
-	token = analex();
-	if(token != identificador){
-		printf("Erro: Esperava identificador");
-		exit(1);
-	}
-	while(token != identificador ){
+
+	while(token == variavel ){
 		AtribuirTipoImplicito();
 		token = analex();
 	}
-	token = analex();
-	if(token != procedimento || token != )
+	while (token == procedimento){
+		AtribuirProcedimento();
+		token = analex();
+	}
+	while (token == funcao){
+		AtribuirFuncao();
+		token = analex();
+	}
+	while (token == inicio || token == fim){
+		while(token == numero){
+			VerificaComando();
+			token = anaLex();
+		}
+	}
     
 }
 
-void  AtribuirLabel(){
-	Token token = anaLex();
+
+void  Atribuirrotulo(){
+	token = anaLex();
 	if (token != numero) {
 		printf("Erro: esperava a palavra numero!");
 		exit(1);
@@ -101,7 +104,7 @@ void  AtribuirLabel(){
 }
 
 void AtribuirVariavel(){
-    Token token = anaLex();
+    token = anaLex();
     if(token != identificador){
         printf("Erro:Esperava um identificador");
         exit(1);
@@ -118,14 +121,24 @@ void AtribuirVariavel(){
         exit(-1);
     }
     token = anaLex();
-    if(token != pontoEVirgula){
+    if(token != pontoevirgula){
         printf("Erro: Esperava um pontoEVirgula");
         exit(-1);
     }
 }
 
 void AtribuirTipoImplicito(){
-	Token token;
+	token;
+	token = analex();
+	if(token != variavel){
+		printf("Erro: esperava-se a uma variavel");
+		exit(1);
+	}
+	token = analex();
+	if(token != identificador){
+		printf("esperava-se um identificador");
+		exit(1);
+	}
 	while( (token= analex()) != virgula ){
 		token  = analex();
 		if(token != identificador){
@@ -149,9 +162,75 @@ void AtribuirTipoImplicito(){
 	}
 }
 
+void AtribuirProcedimento(){
+	token = analex();
+	if(token != abreparenteses || token != EhBloco()){
+		printf("Esperava-se um parametro formal ou um bloco!");
+		exit(1);
+	}
+	if(token == abreparenteses){
+		parametrosFormais(pontoevirgula);
+		}
+	
+	token = analex();
+	if(token != EhBloco()){
+		printf("Esperava-se um bloco!");
+		exit(1);
+	}
+	AtribuirBloco(); //recursão indireta em bloco
+	token = analex();
+	if(token != pontoevirgula){
+		printf("Erro: esperava-se um ponto e virgula");
+		exit(1);
+	}
+}
+
+void AtribuirParametosFormais(Token tokenComparativo){
+	parametrosFormais();
+	token = analex();
+	if(token != tokenComparativo){
+		printf("Erro: esperava-se um ponto e virgula");
+		exit(1);
+	}
+	}
+
+bool EhBloco(){
+	bool Ehrotulo = token == rotulo;
+	bool EhTipo = token == tipo;
+	bool EhImplicito = token == EhImplicito;
+	bool EhProcedimento = token == procedimento;
+	bool EhFuncao = token == funcao;
+	bool EhBegin = token == inicio;
+	return Ehrotulo || EhTipo ||  EhImplicito||EhProcedimento || EhFuncao || EhBegin;
+}
+
+void AtribuirFuncao(){
+	token = analex();
+	if(token != identificador){
+		printf("Erro: esperava-se um identificador");
+		exit(1);
+	}
+	token = analex();
+	if(token != abreparenteses || token != doispontos){
+		printf("Erro: esperava-se um parametro formal ou um dois pontos");
+		exit(1);
+	}
+	AtribuirParametosFormais(doispontos);
+	token = analex();
+	if(token != identificador){
+		printf("Erro: esperava-se um identificador");
+		exit(1);
+	}
+	token = analex();
+	if(token != EhBloco()){
+		printf("Erro: esperava-se um bloco");
+		exit(1);
+	}
+}
+
 void VerificaComandoSemRotulo()
 {
-	token = anaLex();
+	 token = anaLex();
 	if(token == identificador)
 	{
 		token = anaLex();
@@ -269,4 +348,59 @@ void VerificaComando()
 		exit(-1);
 	}
 	VerificaComandoSemRotulo();
+}
+
+void Expressao(){
+	ExpressaoSimples();
+	token = anaLex();
+	if(token == igual || token == diferente || token == menor || token == menorouigual || token == maior || token == maiorouigual){
+		token = anaLex();
+		if(token != identificador){
+			printf("Erro: Esperava-se um identificador");
+			exit(-1);
+		}
+		ExpressaoSimples();
+	}
+}
+
+void ExpressaoSimples(){
+	token = anaLex();
+	if(token != mais && token != menos && token != identificador){
+		printf("Erro: Esperava-se um operador ou um termo");
+		exit(-1);
+	}
+	if(token == mais || token == menos){
+		token = anaLex();
+		if(token != identificador){
+			printf("Erro: Esperava-se um termo");
+			exit(-1);
+		}
+	}
+	termo();
+	token = analex();
+	while(token == mais || token == menos || token == ou){
+		token = analex();
+		if(token != identificador){
+			printf("Erro: Esperava-se um termo");
+			exit(-1);
+		}
+		termo();
+		token = analex();
+	}
+}
+
+void Termo(){
+	if(token != identificador){ // pão durismo, o termo sempre vai começar com um fator que sempre vai começar com um identificador
+		printf("Erro: Esperava-se um identificador");
+		exit(-1);
+	}
+	token = analex();
+	while(token == vezes || token == dividir || token == e){
+		token = analex();
+		if(token != identificador){
+			printf("Erro: Esperava-se um identificador");
+			exit(-1);
+			fator();
+	}
+	
 }
